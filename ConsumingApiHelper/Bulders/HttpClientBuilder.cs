c2ed9fb0372d305ca795d40155912a214be1384f
william silva de santana <master_william_@hotmail.com>
2019-02-19 16:48:06 -0300

    Adicionar arquivos de projeto.

---
+++ ConsumingApiHelper/Bulders/HttpClientBuilder.cs
@@ -0,0 +1,31 @@
+﻿using ConsumingApiHelper.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ConsumingApiHelper.Bulders {
+    public class HttpClientBuilder {
+        public static HttpClient Create(string appHost, string authenticationUri, string username, string password) {
+            var client = new HttpClient() {
+                BaseAddress = new Uri(appHost)
+            };
+            var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(mediaType);
+            var auth = new AuthenticationHelper(client, authenticationUri);
+            var token = auth.Authenticate(username, password);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            return client;
+        }
+
+        public static HttpClient Anonimous(string appHost) {
+            var client = new HttpClient() {
+                BaseAddress = new Uri(appHost)
+            };
+            var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(mediaType);
+            return client;
+        }
+    }
+}
---
+++ ConsumingApiHelper/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsumingApiHelper.Helpers {
+    public class AuthenticationHelper {
+        private HttpClient _client;
+        private string _authenticationUri;
+
+        public AuthenticationHelper(HttpClient client, string authenticationUri) {
+            this._client = client;
+            this._authenticationUri = authenticationUri;
+        }
+
+        public string Authenticate(string username, string password) {
+            string token = null;
+            var data = new Dictionary<string, string>() {
+                    { "username", username },
+                    { "password", password }
+                };
+            using (var requestContent = new FormUrlEncodedContent(data)) {
+                var response = _client.PostAsync(_authenticationUri, requestContent).Result;
+                if (response.IsSuccessStatusCode) {
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    var tokenData = JObject.Parse(responseContent);
+                    token = (string)tokenData["token"];
+                }
+            }
+            return token;
+        }
+    }
+}
